77aa8c2c2f0b86c5d38e3b80a985a1e3c454ae87
Black <florian.evertz@googlemail.com>
2019-04-14 13:55:25 +0200

    change

--- default/module.spawn.js
+++ default/module.spawn.js
@@ -51,7 +51,9 @@ var spawn = {
     spawnCreep: function (spawn, objective, body) {
         var random = Math.floor((Math.random() * 100) + 1);
         var newName = spawn.spawnCreep(this.getCreepBody(body), 'scribbles' + random, {memory: {role: objective}});
-        console.log(JSON.stringify(spawn.spawning));
+        if (spawn.spawning) {
+            console.log(JSON.stringify(spawn.spawning));
+        }
         if (newName === -6) {
             console.log('Not enought Energy');
         } else {
