586413c3d84567fc23d848e0624e26eda511f23b
Florian Evertz <florian.evertz@heidelpay.de>
2019-05-21 09:11:22 +0200

    changed tower strategy

--- default/role.tower.js
+++ default/role.tower.js
@@ -2,28 +2,29 @@
 
 let roleTower = {
 
-    defendLasika: function(){
+    defendLasika: function () {
         let spawn = Game.spawns['Lasika'];
         let towers = spawn.room.find(
             FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
-        if(towers) {
-            /*let targets = spawn.room.find(FIND_STRUCTURES, {
+        if (towers) {
+            let hostiles = spawn.room.find(FIND_HOSTILE_CREEPS);
+            let targets = spawn.room.find(FIND_STRUCTURES, {
                 filter: (structure) => {
-                        return (
-								structure.structureType == STRUCTURE_ROAD ||
-								structure.structureType == STRUCTURE_RAMPART ) &&
-								structure.hits < structure.hitsMax
-                        }
+                    return (
+                        structure.structureType === STRUCTURE_ROAD ||
+                        structure.structureType === STRUCTURE_RAMPART) &&
+                        structure.hits < structure.hitsMax
+                }
             });
-                towers.forEach(function(tower){
-                    if(targets && tower.energy >= tower.energyCapacity / 2) {
+            if (hostiles) {
+                towers.forEach(tower => tower.attack(hostiles[0]));
+            } else {
+                towers.forEach(function (tower) {
+                    if (targets && tower.energy >= tower.energyCapacity / 2) {
                         tower.repair(targets[0])
                     }
-                });*/
-
-            let hostiles = spawn.room.find(FIND_HOSTILE_CREEPS);
-            towers.forEach(tower => tower.attack(hostiles[0]));
-
+                });
+            }
             /*let closestHostile = towers[1].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
             if(closestHostile) {
                 towers[1].attack(closestHostile);
