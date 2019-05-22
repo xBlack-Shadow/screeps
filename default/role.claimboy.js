ee066fbbec643fb1b06487fd116d94b4ea7ca6fe
Florian Evertz <florian.evertz@heidelpay.de>
2019-05-22 08:12:59 +0200

    logging and positioning

--- default/role.claimboy.js
+++ default/role.claimboy.js
@@ -3,16 +3,20 @@
 let roleClaim = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
+    run: function (creep) {
         let target = Game.getObjectById('577b92b60f9d51615fa46f8c');
+        let targetPos = new RoomPosition(25, 25, 'W28S27');
         console.log(target);
         console.log(creep.pos);
+        console.log(targetPos);
         console.log(creep.claimController(target));
-        creep.moveTo(target);
-            if(creep.claimController(target) === ERR_NOT_IN_RANGE) {
-                creep.moveTo(target);
-                console.log(target);
-            }
+        if (creep.pos !== targetPos) {
+            creep.moveTo(targetPos);
+        }
+        if (creep.claimController(target) === ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+            console.log(target);
+        }
     }
 };
 
