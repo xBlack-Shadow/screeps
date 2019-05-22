6f0d867b2e8d44374aaf832c3e492fdc02f2bb95
Florian Evertz <florian.evertz@heidelpay.de>
2019-05-22 08:29:46 +0200

    use claim Body to spawn

--- default/role.claimboy.js
+++ default/role.claimboy.js
@@ -7,6 +7,7 @@ let roleClaim = {
         let target = Game.getObjectById('5bbcab669099fc012e6336fd');
         let targetPos = new RoomPosition(25, 25, 'W28S27');
         let count = 0;
+        console.log(count);
         console.log(target);
         console.log(creep.pos);
         console.log(targetPos);
