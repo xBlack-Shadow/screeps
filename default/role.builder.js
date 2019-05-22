dea790c00b0f13801b3d0af4efb9808ba183f118
Florian Evertz <florian.evertz@heidelpay.de>
2019-05-22 11:08:25 +0200

    what

--- default/role.builder.js
+++ default/role.builder.js
@@ -3,48 +3,45 @@
 let roleBuilder = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-        
+    run: function (creep) {
+
         let roleNext = require('role.roadster');
-        if(creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES) === null){
-            creep.moveTo(new RoomPosition(25, 25, 'W28S27'));
-        }else {
-            /*let targets;
-            _.forEach(Game.rooms, room => {
-                console.log(room.find(FIND_CONSTRUCTION_SITES));
-                targets = room.find(FIND_CONSTRUCTION_SITES);
-            });*/
-            let targets = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
+        let targets = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
 
-            if (creep.memory.source === undefined) {
-                let daSources = creep.room.find(FIND_SOURCES_ACTIVE);
-                creep.memory.source = daSources[0].id;
-            }
+        if (creep.memory.source === undefined) {
+            let daSources = creep.room.find(FIND_SOURCES_ACTIVE);
+            creep.memory.source = daSources[0].id;
+        }
 
-            if (creep.memory.building && creep.carry.energy === 0) {
-                creep.memory.building = false;
-            }
-            if (!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
-                creep.memory.building = true;
-            }
+        if (creep.memory.building && creep.carry.energy === 0) {
+            creep.memory.building = false;
+        }
+        if (!creep.memory.building && creep.carry.energy === creep.carryCapacity) {
+            creep.memory.building = true;
+        }
+        /*let targets;
+        _.forEach(Game.rooms, room => {
+            console.log(room.find(FIND_CONSTRUCTION_SITES));
+            targets = room.find(FIND_CONSTRUCTION_SITES);
+        });*/
 
-            if (creep.memory.building) {
-                if (targets/*.length*/) {
-                    if (creep.build(targets/*[0]*/) === ERR_NOT_IN_RANGE) {
-                        creep.moveTo(targets/*[0]*/);
-                    }
-                } else {
-                    roleNext.run(creep);
+
+        if (creep.memory.building) {
+            if (targets !== null) {
+                if (creep.build(targets/*[0]*/) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets/*[0]*/);
                 }
-            }
-            else {
-                let sources = Game.getObjectById(creep.memory.source);
-                if (creep.harvest(sources) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources);
-                }
+            } else {
+                creep.moveTo(new RoomPosition(25, 25, 'W28S27'));
             }
         }
-	}
+        else {
+            let sources = Game.getObjectById(creep.memory.source);
+            if (creep.harvest(sources) === ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources);
+            }
+        }
+    }
 };
 
 module.exports = roleBuilder;
